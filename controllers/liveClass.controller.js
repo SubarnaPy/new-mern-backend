8ed171962e0c3f96cbdea904072249c4921a0b8f
Subarna Mondal <mondalsubarna29@gmail.com>
2025-05-01 12:21:38 +0530

    Initial commit

--- controllers/liveClass.controller.js
+++ controllers/liveClass.controller.js
@@ -1,119 +1,46 @@
-// server.js (Enhanced Backend)
-import { Server } from "socket.io";
-
-let liveClasses = {};
-let userRoles = {};
+import shortid from 'shortid';
 
 export const connectToSocket = (io) => {
-   
+    const users = {};
 
-    io.use((socket, next) => {
-        // Add authentication middleware here (JWT verification)
-        const token = socket.handshake.auth.token;
-        // Verify token and fetch user role from LMS system
-        next();
+    io.on('connection', (socket) => {
+      socket.on('join-room', (roomId) => {
+        if (!users[roomId]) users[roomId] = [];
+    
+        users[roomId].push(socket.id);
+        socket.join(roomId);
+    
+        const otherUsers = users[roomId].filter(id => id !== socket.id);
+        socket.emit('all-users', otherUsers);
+    
+        socket.to(roomId).emit('user-joined', socket.id);
+    
+        socket.on('disconnect', () => {
+          users[roomId] = users[roomId].filter(id => id !== socket.id);
+          socket.to(roomId).emit('user-disconnected', socket.id);
+        });
+    
+        socket.on('offer', (data) => {
+          socket.to(data.target).emit('offer', {
+            sdp: data.sdp,
+            caller: socket.id,
+          });
+        });
+    
+        socket.on('answer', (data) => {
+          socket.to(data.target).emit('answer', {
+            sdp: data.sdp,
+            responder: socket.id,
+          });
+        });
+    
+        socket.on('ice-candidate', (data) => {
+          socket.to(data.target).emit('ice-candidate', {
+            candidate: data.candidate,
+            sender: socket.id,
+          });
+        });
+      });
     });
-
-    io.on("connection", (socket) => {
-        socket.on("create-class", (classData) => {
-            const classId = generateClassId();
-            liveClasses[classId] = {
-                teacher: socket.id,
-                students: new Set(),
-                handRaised: new Set(),
-                isRecording: false,
-                classInfo: classData
-            };
-            userRoles[socket.id] = 'teacher';
-            socket.emit('class-created', classId);
-        });
-
-        // Modify join-class handler to notify all participants
-socket.on("join-class", (classId, userData) => {
-    if (!liveClasses[classId]) return socket.emit('invalid-class');
     
-    const role = liveClasses[classId].teacher === socket.id ? 'teacher' : 'student';
-    userRoles[socket.id] = role;
-    
-    liveClasses[classId].students.add(socket.id);
-    socket.join(classId);
-    
-    // Notify all participants about new user
-    io.to(classId).emit('participant-joined', socket.id);
-    
-    // Send existing participants to new user
-    const participants = [
-      liveClasses[classId].teacher,
-      ...Array.from(liveClasses[classId].students)
-    ].filter(id => id !== socket.id);
-    
-    socket.emit('existing-participants', participants);
-  });
-
-        socket.on("raise-hand", (classId) => {
-            if (userRoles[socket.id] === 'student') {
-                liveClasses[classId].handRaised.add(socket.id);
-                io.to(liveClasses[classId].teacher).emit('hand-raised', socket.id);
-            }
-        });
-
-        socket.on("mute-user", (classId, targetId) => {
-            if (userRoles[socket.id] === 'teacher') {
-                io.to(targetId).emit('force-mute');
-            }
-        });
-
-        socket.on("start-recording", (classId) => {
-            if (userRoles[socket.id] === 'teacher') {
-                liveClasses[classId].isRecording = true;
-                io.to(classId).emit('recording-started');
-            }
-        });
-
-        socket.on("end-class", (classId) => {
-            if (userRoles[socket.id] === 'teacher') {
-                io.to(classId).emit('class-ended');
-                delete liveClasses[classId];
-            }
-        });
-        socket.on("offer", ({ targetId, offer }) => {
-          io.to(targetId).emit("offer", {
-              senderId: socket.id,
-              offer,
-          });
-      });
-      
-      socket.on("answer", ({ targetId, answer }) => {
-          io.to(targetId).emit("answer", {
-              senderId: socket.id,
-              answer,
-          });
-      });
-      
-      socket.on("ice-candidate", ({ targetId, candidate }) => {
-          io.to(targetId).emit("ice-candidate", {
-              senderId: socket.id,
-              candidate,
-          });
-      });
-      
-
-        socket.on("disconnect", () => {
-            for (const classId in liveClasses) {
-                if (liveClasses[classId].teacher === socket.id) {
-                    io.to(classId).emit('class-ended');
-                    delete liveClasses[classId];
-                } else if (liveClasses[classId].students.has(socket.id)) {
-                    liveClasses[classId].students.delete(socket.id);
-                    io.to(liveClasses[classId].teacher).emit('student-left', socket.id);
-                }
-            }
-        });
-    });
-
-    return io;
 };
-
-function generateClassId() {
-    return Math.random().toString(36).substr(2, 9);
-}
